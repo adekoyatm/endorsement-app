7a4b885bb8cf5763b87bf0c1708cebebd720ca25
adekoyatm <adekoyatm@gmail.com>
2024-06-30 18:15:28 +0100

    Initial Commit
    An app that gets endorsements (texts) input in the text area, push the text to the Firebase database, get the array from the database and outputs the input value.
    It is created with HTML, CSS, JavaScript and Firebase database.

---
+++ index.js
@@ -0,0 +1,69 @@
+// javascript
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+    databaseURL: "https://endorsementapp-32ede-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const endorsementListInDB = ref(database, "endorsementList")
+
+const endorsementInputEl = document.getElementById("endorsement-input-el")
+const publishBtn = document.getElementById("publish-btn")
+const endorsementListEl = document.getElementById("endorsement-list")
+
+
+publishBtn.addEventListener("click", function(){
+    
+    let inputValue = endorsementInputEl.value  
+        
+    push(endorsementListInDB, inputValue)
+    
+    clearEndorsementInputEl()       
+})
+
+onValue(endorsementListInDB, function(snapshot){
+    
+    if (snapshot.exists()){
+        
+        let allEndorcementArrayInDB = Object.entries(snapshot.val())
+    
+        clearCurrentlyDisplayedEndorcementList()
+        
+        for (let i = 0; i < allEndorcementArrayInDB.length; i++){
+            let singleEndorcmentArrayInDB = allEndorcementArrayInDB[i]
+            // let singleEndorcmentArrayID = singleEndorcmentArrayInDB[0]
+            // let singleEndorcmentArrayValue = singleEndorcmentArrayInDB[1]
+                    
+            appendEndorcementInputToEndorementList(singleEndorcmentArrayInDB)
+        }
+    } else{
+        endorsementListEl.innerHTML =""
+    }            
+})
+
+
+function clearEndorsementInputEl(){
+    endorsementInputEl.value  ="" 
+}
+
+function clearCurrentlyDisplayedEndorcementList(){
+    endorsementListEl.innerHTML ="" 
+}
+
+function appendEndorcementInputToEndorementList(endorcement) {
+    let newEndorcemntID = endorcement[0]
+    let newEndorcemntValue = endorcement[1]
+    
+    let newEndorcemnt = document.createElement("li")
+    newEndorcemnt.textContent = newEndorcemntValue
+    
+    newEndorcemnt.addEventListener("dblclick", function(){
+        remove( ref(database, `endorsementList/${newEndorcemntID}`) )
+    })
+    endorsementListEl.append(newEndorcemnt)
+}
+   
